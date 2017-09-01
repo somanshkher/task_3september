337590d6299841ae2f7b46a070b478035ab582b6
somanshkher <31530574+somanshkher@users.noreply.github.com>
2017-09-01 11:02:06 +0530

    task1 and 2

---
+++ pattern1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author S020012001
+ */
+public class pattern1
+{
+    public static void main(String []arg) 
+    {   
+           int i,j;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }    
+            }
+        }
+               
+    }
+}
